9370abea35403562d99f7d031de71e3d4437751e
Derek Chen <derekgc@icloud.com>
2018-05-15 10:47:05 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,31 @@
 require_relative 'config/environment'
+require 'sinatra/base'
+
+require "pry"
 
 class App < Sinatra::Base
+
+
+
+
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+
+    # binding.pry
+    erb :create_puppy
+  end
+
+  post '/' do
+    @name = params["name"]
+    @age = params["age"]
+    @breed = params["breed"]
+
+    erb :display_puppy
+  end
+
+
 
 end
